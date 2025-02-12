c397e6ca254c7bccd5548c641b204511f2639c35
thinkGrow <rubaitreshad@gmail.com>
2025-02-13 00:00:00 +0600

    Tailwind CLI and Fontawesome CDN and Import Font

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./*.html"], // Adjust the path to match your project files
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+      },
+    },
+  },
+  plugins: [],
+};
