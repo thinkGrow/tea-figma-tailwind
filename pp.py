d98549e2f4dea8d14927fbea5db8e42459a4e541
thinkGrow <rubaitreshad@gmail.com>
2025-02-20 03:58:17 +0600

    hero section done

---
+++ pp.py
@@ -0,0 +1,12 @@
+print("Please enter the first number")
+
+firstNumber = int(input())
+
+print("Please enter the second number")
+
+secondNumber = int(input())
+
+
+total = firstNumber + secondNumber
+
+print(total)
